ac8dd0647112dd257085f9d43b732fccf4b3e28b
slTrust <25893245+slTrust@users.noreply.github.com>
2019-06-19 13:43:03 +0800

    一个合法的十六进制的字符 (#14)
    一个合法的十六进制的字符 (#14)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -2,12 +2,25 @@ package com.github.hcsp.calculation;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
+        System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('a'));
         System.out.println("0是合法的十六进制字符:" + isValidHexCharacter('0'));
         System.out.println("G是合法的十六进制字符:" + isValidHexCharacter('G'));
     }
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        /*
+        48  0
+        57  9
+
+        65  A
+
+        70  F
+
+        97  a
+         */
+        int val = (int) ch;
+        return (48<= val && val<=57)||(65<= val && val<=70)||(97<= val && val<=102);
+    }
 }
