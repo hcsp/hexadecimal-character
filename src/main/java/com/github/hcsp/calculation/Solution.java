4b3c29370f7a64572fe23e643a8e38e52839cba6
Procarihana <48359478+Procarihana@users.noreply.github.com>
2019-07-14 00:58:20 +0800

    编写一个方法，给定一个字符 (#34)
    编写一个方法，给定一个字符 (#34)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.util.ArrayList;
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
@@ -9,5 +11,17 @@ public class Solution {
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+
+    public static boolean isValidHexCharacter(char ch) {
+        byte bch = (byte) ch;
+        String str = "0123456789AaBbCcDdEeFf";
+        for (int i = 0; i < str.length(); i++) {
+            int b = (int) str.charAt(i);
+            int d = b ^ bch;
+            if (d == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
