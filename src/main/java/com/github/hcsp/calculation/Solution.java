47853d1f8454681c39076a1b317d70d76e49458a
yueyue chou <13444339+yueyuezone@users.noreply.github.com>
2019-08-01 23:06:31 +0800

    匹配char (#45)
    匹配char (#45)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.calculation;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
@@ -9,5 +13,11 @@ public class Solution {
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        String str = ""+ch;
+         String pattern = ("\\d|[A-Fa-f]");
+        boolean isMatch = Pattern.matches(pattern,str);
+        return isMatch;
+
+    }
 }
