857bda15719ece46db1a1d0088a987bfbf7e6806
silvia-YQY <545067444@qq.com>
2019-08-05 17:03:42 +0800

    regex (#46)
    regex (#46)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -1,4 +1,5 @@
 package com.github.hcsp.calculation;
+import java.util.regex.*;
 
 public class Solution {
     public static void main(String[] args) {
@@ -9,5 +10,10 @@ public class Solution {
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        String regex="^[A-Fa-f0-9]+$";
+        String s = String.valueOf(ch);
+        boolean isValid = s.matches(regex);
+        return isValid;
+    }
 }
