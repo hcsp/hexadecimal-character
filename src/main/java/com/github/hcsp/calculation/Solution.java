aaf744a5652fbd976d7371025fda98ee583c571a
qiaomengnan16 <285921957@qq.com>
2019-10-01 09:40:42 +0800

    char (#77)
    char (#77)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -9,5 +9,13 @@ public class Solution {
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        char [] chars = new char[] {'0','1','2','3','4','5','6','7','8','9','A','a','B','b','C','c','D','d','E','e','F','f'};
+        for(char temp : chars) {
+            if(temp == ch)
+                return true;
+        }
+        return false;
+    }
+
 }
