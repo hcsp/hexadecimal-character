637cc8f68a7fcb56755c87d312286f2de93215c9
Ditto0 <46198155+Ditto0@users.noreply.github.com>
2019-11-01 00:20:50 +0800

    1

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -2,6 +2,7 @@ package com.github.hcsp.calculation;
 
 public class Solution {
     public static void main(String[] args) {
+
         System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
         System.out.println("0是合法的十六进制字符:" + isValidHexCharacter('0'));
         System.out.println("G是合法的十六进制字符:" + isValidHexCharacter('G'));
@@ -9,5 +10,12 @@ public class Solution {
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        if(ch>=48 && ch<=57)
+            return true;
+        else{
+            ch =Character.toLowerCase(ch);
+            return (ch>= 97 && ch<=102);
+        }
+    }
 }
