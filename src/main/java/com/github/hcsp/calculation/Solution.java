a7b22572bd1dfaf7ead4761a7c11b7004faffcef
chenxiaolani <2524729719@qq.com>
2020-01-09 22:40:55 +0800

    使用list中contain方法查找指定字符

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.calculation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
@@ -9,5 +13,8 @@ public class Solution {
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        List<Character> collection = Arrays.asList('0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','A','B','C','D','E','F');
+        return collection.contains(ch);
+    }
 }
