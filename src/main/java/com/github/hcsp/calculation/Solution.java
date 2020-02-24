3e001a655c9e9346a8ffeb59acfe306af1759578
breeze <1023349760@qq.com>
2020-02-24 22:58:57 +0800

    完成判断

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -5,9 +5,20 @@ public class Solution {
         System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
         System.out.println("0是合法的十六进制字符:" + isValidHexCharacter('0'));
         System.out.println("G是合法的十六进制字符:" + isValidHexCharacter('G'));
-    }
+}
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+         int ch1  = ch;
+        if ((int)'0'<=ch1&&ch1<=(int)'9'){
+            return true;
+        }else if ((int)'A'<=ch1&&ch1<=(int)'F'){
+            return true;
+        }else if ((int)'a'<=ch1&&ch1<=(int)'f'){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
