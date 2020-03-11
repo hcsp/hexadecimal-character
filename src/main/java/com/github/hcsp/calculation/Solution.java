703cd6e43ef3e19503728b9faa396ae3971541bd
llaobin0207 <llaobin0207@163.com>
2020-03-11 08:43:22 +0800

    判断一个字符是不是十六进制字符

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -10,14 +10,12 @@ public class Solution {
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
     public static boolean isValidHexCharacter(char ch) {
-        if(ch >='0' && ch <= '9' ){
-            return true;
-        }else if (ch >='A' && ch <='Z'){
-            return true;
-        }else if (ch >='a' && ch <= 'z'){
-            return true;
-        }else {
-            return false;
-        }
+
+            if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F')) {
+                return true;
+            } else {
+                return false;
+            }
+
     }
 }
