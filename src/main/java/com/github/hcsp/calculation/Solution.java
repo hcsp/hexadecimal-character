20366d62630b0509dbffe15e65f4924d7211806c
hanchenhao <hanchenhao@vip.qq.com>
2020-05-17 15:23:09 +0800

    ASCII (#203)
    ASCII (#203)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -3,11 +3,13 @@ package com.github.hcsp.calculation;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
-        System.out.println("0是合法的十六进制字符:" + isValidHexCharacter('0'));
         System.out.println("G是合法的十六进制字符:" + isValidHexCharacter('G'));
+
     }
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        return (ch >= 'A'&& ch <= 'F') || (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f');
+    }
 }
