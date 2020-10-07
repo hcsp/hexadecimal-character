3bcf3b238086d91a117fab8d244c8334dc1a385c
catmmao <catmmao@gmail.com>
2020-10-07 18:35:26 +0800

    Update Solution.java
    update

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -10,7 +10,8 @@ public class Solution {
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
     public static boolean isValidHexCharacter(char ch) {
-        int asciiCode = ch;
-        return (asciiCode >= 48 && asciiCode <= 57) || (asciiCode >= 65 && asciiCode <= 70) || (asciiCode >= 97 && asciiCode <= 102);
+          return (ch >= '0' && ch <= '9')
+                || (ch >= 'a' && ch <= 'f')
+                || (ch >= 'A' && ch <= 'F');
     }
 }
