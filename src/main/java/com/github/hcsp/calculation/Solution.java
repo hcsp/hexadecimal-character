53afba1aab3f37b8ce31d743f9b2c106d25f6e9b
JerryWang <1976883731@qq.com>
2021-10-30 17:59:31 +0800

    Fix bug

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -2,7 +2,7 @@ package com.github.hcsp.calculation;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('A'));
+        System.out.println("A是合法的十六进制字符:" + isValidHexCharacter('E'));
         System.out.println("0是合法的十六进制字符:" + isValidHexCharacter('0'));
         System.out.println("G是合法的十六进制字符:" + isValidHexCharacter('G'));
     }
@@ -10,6 +10,7 @@ public class Solution {
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
     public static boolean isValidHexCharacter(char ch) {
-        return (((int) ch >= 48) && ((int) ch <= 57)) || (((int) ch >= 65) && ((int) ch <= 70)) || (((int) ch >= 97) && ((int) ch <= 102));
+//        return (((int) ch >= 48) && ((int) ch <= 57)) || (((int) ch >= 65) && ((int) ch <= 70)) || (((int) ch >= 97) && ((int) ch <= 102));
+        return (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'F') || (ch >= 'a' && ch <= 'f');
     }
 }
