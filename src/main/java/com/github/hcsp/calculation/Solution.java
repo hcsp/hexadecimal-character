363514411a4c4a3639ce33f17e305f09a3f40f0b
oxc <524112857@qq.com>
2022-09-06 11:58:30 +0800

    isValidHexCharacter

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -9,5 +9,11 @@ public class Solution {
 
     // 一个合法的十六进制的字符是：字符0-9，以及字符A/a/B/b/C/c/D/d/E/e/F/f （大小写都是合法的）
     // 编写一个方法，给定一个字符，若是合法的十六进制字符，返回true，否则返回false
-    public static boolean isValidHexCharacter(char ch) {}
+    public static boolean isValidHexCharacter(char ch) {
+        boolean isNumber = (int)ch >= 48 && (int)ch <= 57;
+        boolean isLowerWord = (int)ch >= 97 && (int)ch <= 107;
+        boolean isUpperWord = (int)ch >= 65 && (int)ch <= 70;
+
+        return isNumber || isLowerWord || isUpperWord;
+    }
 }
